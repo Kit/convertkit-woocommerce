802e8051b65eecb9f612ef626af8ca4fdf6da945
Tim Carr <tim@n7studios.co.uk>
2025-01-22 15:16:50 +0800

    Added test for checkout opt-in block in WooCommerce 8.4.0

--- tests/acceptance/backward-compat/BackwardCompatSettingsOptInCheckboxCest.php
+++ tests/acceptance/backward-compat/BackwardCompatSettingsOptInCheckboxCest.php
@@ -4,7 +4,7 @@
  *
  * @since   1.9.1
  */
-class SettingOptInCheckboxCest
+class BackwardCompatSettingOptInCheckboxCest
 {
 	/**
 	 * Run common actions before running the test functions in this class.
@@ -28,7 +28,53 @@ class SettingOptInCheckboxCest
 		$I->loadConvertKitSettingsScreen($I);
 	}
 
-	
+	/**
+	 * Test that the opt in checkbox block is automatically added to the WooCommerce Checkout
+	 * block, cannot be removed and links to the integration settings screen.
+	 *
+	 * @since   1.9.1
+	 *
+	 * @param   AcceptanceTester $I  Tester.
+	 */
+	public function testOptInCheckboxBlockInEditor(AcceptanceTester $I)
+	{
+		// Enable the Opt-In Checkbox option.
+		$I->checkOption('#woocommerce_ckwc_display_opt_in');
+
+		// Save.
+		$I->click('Save changes');
+
+		// Edit Checkout Page.
+		$pageID = $I->grabFromDatabase(
+			'wp_posts',
+			'ID',
+			[
+				'post_name' => 'checkout',
+			]
+		);
+		$I->amOnAdminPage('post.php?post=' . $pageID . '&action=edit');
+
+		// Close Gutenberg modal.
+		$I->maybeCloseGutenbergWelcomeModal($I);
+
+		// Confirm Checkout Block exists in Checkout.
+		$I->click('div[data-type="ckwc/opt-in"]');
+
+		// Confirm block cannot be deleted.
+		$I->click('button.components-dropdown-menu__toggle');
+		$I->waitForElementVisible('.components-popover');
+		$I->dontSee('Unlock', '.components-popover');
+		$I->dontSee('Delete', '.components-popover');
+
+		// Confirm Configure Opt In button.
+		$I->click('Configure Opt In');
+
+		// Switch to tab that opened.
+		$I->switchToNextTab();
+
+		// Confirm this displays the integration settings screen.
+		$I->seeCheckboxIsChecked('#woocommerce_ckwc_display_opt_in');
+	}
 
 	/**
 	 * Deactivate and reset Plugin(s) after each test, if the test passes.
