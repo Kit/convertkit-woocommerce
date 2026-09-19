7eaa9c5c8413ff529be492346b51e0ed42fa748e
Tim Carr <tim@n7studios.co.uk>
2026-09-19 10:53:37 +0800

    Use Subscriber ID from Purchase Data (#319)
    * Tests: Fix Subscriber Checks
    
    * Coding standards
    
    * Wait for subscriber with custom field data before assertion
    
    * Coding standards
    
    * Coding standards for tests
    
    * Purchase Data: Use subscriber ID from result instead of looking up by email
    
    * Tests: Logs: Fetch subscriber ID from purchase data response instead of email lookup
    
    * Coding standards
    
    * Purchases: Check for subscriber ID
    
    * Isolate failing test
    
    * Reinstate close modal
    
    * Reinstate all tests
    
    * Fix typo
    
    * Fix Sync Past Order Tests

--- tests/Support/Helper/WPGutenberg.php
+++ tests/Support/Helper/WPGutenberg.php
@@ -10,6 +10,32 @@ namespace Tests\Support\Helper;
 class WPGutenberg extends \Codeception\Module
 {
 	/**
+	 * Helper method to switch to the Gutenberg editor Iframe
+	 * when all blocks use the Block API v3:
+	 * https://developer.wordpress.org/block-editor/reference-guides/block-api/block-api-versions/
+	 *
+	 * @since   2.7.7
+	 *
+	 * @param   EndToEndTester $I  EndToEnd Tester.
+	 */
+	public function switchToGutenbergIFrameEditor($I)
+	{
+		// Wait for the iframe to mount. In WordPress 7.1+ the editor iframe
+		// can take longer to appear in the DOM than a tryToSeeElement()
+		// check, causing the switch to be silently skipped. Catch the timeout
+		// so that environments which intentionally have no iframe (e.g. Divi
+		// is active, which prevents the iframe block editor from being used)
+		// still fall through to the non-iframe path.
+		try {
+			$I->waitForElement('iframe[name="editor-canvas"]', 10);
+		} catch (\Facebook\WebDriver\Exception\TimeoutException $e) {
+			return;
+		}
+
+		$I->switchToIFrame('iframe[name="editor-canvas"]');
+	}
+
+	/**
 	 * Helper method to close the Gutenberg "Welcome to the block editor" dialog, which
 	 * might show for each Page/Post test performed due to there being no persistence
 	 * remembering that the user dismissed the dialog.
